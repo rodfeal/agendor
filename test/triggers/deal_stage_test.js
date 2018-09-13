d628ba6a0aaeefcb0571c709f9e8dcc853416866
juniormesquitadandao <juniormesquitadandao@gmail.com>
2018-09-13 09:32:16 -0300

    triggers

---
+++ test/triggers/deal_stage_test.js
@@ -0,0 +1,26 @@
+const plg = require('pluga-plg');
+const expect = require('chai').expect;
+const trigger = require('../../lib/triggers/deal_stage');
+
+describe('Trigger: deal stage', () => {
+  it('test your trigger handle here', (done) => {
+    const event = {
+      meta: {
+        baseURI: process.env.BASE_URI
+      },
+      auth: {
+        token: process.env.TOKEN
+      },
+      input: {
+        dealStage: 1446424
+      }
+    };
+
+    trigger.handle(plg, event).then((results) => {
+      results.forEach((deal) => {
+        expect(deal.dealStage.id).to.eq(1446424);
+      });
+      done();
+    }).catch(done);;
+  });
+});
