d2a47eda2e0e697b65052fa06d961611045ea3e3
renanmac <renanmacs@gmail.com>
2018-09-17 15:03:15 -0300

    Add deal fields

--- test/actions/add_deal_test.js
+++ test/actions/add_deal_test.js
@@ -2,24 +2,36 @@ const plg = require('pluga-plg');
 const expect = require('chai').expect;
 const action = require('../../lib/actions/add_deal');
 
+const title = `Deal Test-${Date.now()}`;
+const event = {
+  meta:{
+    baseURI: process.env.BASE_URI
+  },
+  auth: {
+    api_key: process.env.TOKEN
+  },
+  input:{
+    organization_id: 15022132,
+    title: title,
+    type: 'organization'
+  }
+};
+
 describe('Action: Add deal', () => {
   it('Should add a deal', function (done) {
-    const event = {
-      meta:{
-        baseURI: process.env.BASE_URI
-      },
-      auth: {
-        api_key: process.env.TOKEN
-      },
-      input:{
-        organization_id: 15022132,
-        title: 'Deal Test'
-      }
-    };
-    debugger
+
     action.handle(plg, event)
-      .then((res) => {
-        expect(res.status).to.eq(201);
+      .then(res => {
+        expect(res.statusText).to.eq('Created');
+        done();
+      })
+      .catch(done);
+  });
+
+  it('Should update a deal', function (done) {
+    action.handle(plg, event)
+      .then(res => {
+        expect(res.statusText).to.eq('OK');
         done();
       })
       .catch(done);
